766b0795f8b0f2e5c2d7797ab120934dc6549a3a
Marcos8060 <marcosgav80@gmail.com>
2024-12-09 08:22:42 +0300

    intergrated vue router

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <script setup>
 import Navbar from "./components/Navbar.vue";
-import Hero from "./components/Hero.vue";
-import HomeCard from "./components/HomeCards.vue";
-import JobListings from "./components/JobListings.vue";
+import { RouterView } from "vue-router";
+
 </script>
 
 
 
 <template>
   <Navbar />
-  <Hero title="Test Title" />
-  <HomeCard />
-  <JobListings />
+  <RouterView />
 </template>
--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -4,6 +4,14 @@ import jobsData from "../../src/jobs.json";
 import JobList from "./JobList.vue";
 
 const jobs = ref(jobsData);
+
+defineProps({
+  limit: Number,
+  showButton:{
+    type: Boolean,
+    default: false
+  }
+});
 </script>
 
 <template>
@@ -13,8 +21,19 @@ const jobs = ref(jobsData);
         Browse Jobs
       </h2>
       <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-        <JobList v-for="job in jobs" :key="job.id" :job="job" />
+        <JobList
+          v-for="job in jobs.slice(0, limit || jobs.length)"
+          :key="job.id"
+          :job="job"
+        />
       </div>
     </div>
+  </section>
+  <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
+    <a
+      href="jobs.html"
+      class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+      >View All Jobs</a
+    >
   </section>
 </template>
